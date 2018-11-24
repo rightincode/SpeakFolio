7acfc1efe6b09b99887eaf3024599d888013ff33
Richard Taylor <rtaylor@rightincode.com>
2018-11-24 11:38:29 -0500

    Add project files.

---
+++ SpeakFolio.Identity/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SpeakFolio.Identity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
