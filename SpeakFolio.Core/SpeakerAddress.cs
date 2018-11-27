63e2acd3512b09a990bc1148fb07fec8ab2aa1fe
Richard Taylor <rtaylor@rightincode.com>
2018-11-26 22:01:01 -0500

    Creation of SpeakerProfile and SpeakerAddress domain objects

---
+++ SpeakFolio.Core/SpeakerAddress.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SpeakFolio.Core
+{
+    public class SpeakerAddress
+    {
+        public int Id { get; set; }
+
+        public string Address1 { get; set; }
+
+        public string Address2 { get; set; }
+
+        public string Address3 { get; set; }
+
+        public string City { get; set; }
+
+        public string County { get; set; }
+
+        public string StateProvience { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public string Country { get; set; }
+
+        public int SpeakerProfileId { get; set; }
+
+        public Guid Uid { get; set; }
+    }
+}
---
+++ SpeakFolio.Core/SpeakerProfile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpeakFolio.Core
+{
+    public class SpeakerProfile
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        
+        public string EmailAddress { get; set; }
+
+        public string TwitterHandle { get; set; }
+
+        public string TwitterUrl { get; set; }
+
+        public string LinkedInUrl { get; set; }
+
+        public string FaceBookUrl { get; set; }
+
+        public string YouTubeChannelUrl { get; set; }
+
+        public string GitHubUrl { get; set; }
+
+        public List<SpeakerAddress> Addresses { get; set; }
+
+        public string AspNetUserId { get; set; }
+
+        public Guid Uid { get; set; }
+    }
+}
